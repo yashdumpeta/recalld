3cc974561821d870e08ff17fc9bddbded2c6e14e
yashdumpeta <yashcollege7777@gmail.com>
2024-06-26 00:22:35 -0400

    finished for today

--- flashcard_app/catalog/models.py
+++ flashcard_app/catalog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Deck(models.Model):
+    deck_name = models.CharField(blank=False, max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pass
