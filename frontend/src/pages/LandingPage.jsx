d1bd6b5d596d78084bc30b5cd5784a43c870f3c6
yashdumpeta <yashcollege7777@gmail.com>
2024-06-30 17:47:31 -0400

    added a basic landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../styles/LandingPage.css'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className='page-container'>
+      <div className='content-wrapper'>
+        <h1 id='slogan'>Master your memory.</h1>
+        <h2 id='slogan-description'>A flashcard app empowering you with the tools to retain your information.</h2>
+        <div className='cta-container'>
+          <Link id='register-button' to='/register' className="register-button">Get recalld for free</Link>
+        </div>
+      </div>
     </div>
   )
 }
