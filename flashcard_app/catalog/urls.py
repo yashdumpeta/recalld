a075a1679ccea54e23c3918a6d293398ce01d566
yashdumpeta <yashcollege7777@gmail.com>
2024-07-07 17:20:58 -0400

    made some adjustments to deck retrieve and delete view functions as well as some card functions

--- flashcard_app/catalog/urls.py
+++ flashcard_app/catalog/urls.py
@@ -10,7 +10,7 @@ from . import views
 urlpatterns = [
     path('register/', views.CreateUserView.as_view(), name='register'),
     path("decks/", views.DeckCreate.as_view(), name="deck-list-create"),  # URL for creating or listing decks
-    path("decks/<int:pk>/", views.DeckDelete.as_view(), name="deck-delete"),  # URL for deleting a specific deck
+    path("decks/<int:pk>/delete/", views.DeckDelete.as_view(), name="deck-delete"),
     path('decks/<int:pk>/', views.DeckRetrieve.as_view(), name='deck-retrieve'),
     path('decks/<int:pk>/update/', views.DeckUpdate.as_view(), name='deck-update'),
     path('decks/<int:pk>/delete/', views.DeckDelete.as_view(), name='deck-delete'),
